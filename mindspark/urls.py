5b91ef8223031d64ce3712f3ff913adec5100f62
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2024-10-02 21:52:27 +0100

    Created static file directory

--- mindspark/urls.py
+++ mindspark/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('accounts/', include("accounts.urls"), name="accounts"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
